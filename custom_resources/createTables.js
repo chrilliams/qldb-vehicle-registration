a15de5a69c8fc557fc4db4b570b7af98a5da4c28
Chris Williams <Chris.Williams@dvla.gov.uk>
2019-12-22 22:19:05 +0000

    QLDB create

---
+++ custom_resources/createTables.js
@@ -0,0 +1,54 @@
+'use strict';
+
+const response = require('cfn-response-promise');
+const { QldbSession, Result, TransactionExecutor } = require('amazon-qldb-driver-nodejs')
+const { closeQldbSession, createQldbSession } = require('./QLDBHelpers/ConnectToLedger');
+
+module.exports.handler = async (event,context) => {
+  try {
+
+    if (event.RequestType === 'Create' || event.RequestType === 'Update') {
+
+      let session;
+      try {
+          session = await createQldbSession();
+          await session.executeLambda(async (txn) => {
+            Promise.all([
+                createTable(txn, 'VehicleRegistration'),
+                createTable(txn, 'Vehicle'),
+                createTable(txn, 'Person'),
+                createTable(txn, 'DriversLicense')
+            ]);
+        }, () => log("Retrying due to OCC conflict..."));
+      } catch (e) {
+          console.log(`Unable to connect: ${e}`);
+          throw e;
+      } finally {
+          closeQldbSession(session);
+      }
+
+
+    }
+
+    var responseData = {'requestType': event.RequestType};
+    await response.send(event, context, response.SUCCESS, responseData);
+  }
+  catch(error) {
+    console.error(error);
+
+    await response.send(event, context, response.FAILED, {'Error': error});
+
+  }
+
+};
+
+
+async function createTable(txn, tableName){
+  const statement = `CREATE TABLE ${tableName}`;
+  try {
+    const result = await txn.executeInline(statement);
+    return result.getResultList().length;
+  } catch (e) {
+    console.log(`error: ${e}`);
+  }
+}
